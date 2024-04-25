3f7d1a9fb47224a021f5b57b49c4b473886fbab3
Bret <bsolidgold@gmail.com>
2024-04-25 16:38:48 -0600

    Initial commit

---
+++ src/main/java/org/bretgold/Selenium_Demo.java
@@ -0,0 +1,19 @@
+package org.bretgold;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+public class Selenium_Demo {
+
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+
+    public void setUp() {
+        System.out.print("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+
+}
